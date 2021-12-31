2e932d99831d5d6f5420077e48809fcc8653e6ed
MarioZDevelop <mariozdesarrollo@gmail.com>
2021-12-31 13:23:36 -0500

    Listos puntos 1,2,3 6,7 y 8, falta un poquito del 4 y del 5

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,6 +1,6 @@
 class DocumentsController < ApplicationController
   before_action :set_document, only: %i[ show edit update destroy ]
-
+  
   # GET /documents or /documents.json
   def index
     @documents = Document.all
@@ -8,6 +8,7 @@ class DocumentsController < ApplicationController
 
   # GET /documents/1 or /documents/1.json
   def show
+    @workflow_state = Document.find(params[:id]).workflow_state
   end
 
   # GET /documents/new
@@ -19,15 +20,27 @@ class DocumentsController < ApplicationController
   def edit
   end
 
+  # PUT /documents/1/update_workflow
+  def update_workflow_state
+    @workflow_state = Document.find(params[:id]).workflow_state
+    if @workflow_state == 'Draft'
+      @workflow_state = 'awaiting_review'
+    elsif @workflow_state == 'awaiting_review'
+      @workflow_state = 'awaiting_approve'
+    elsif @workflow_state == 'awaiting_approve'
+      @workflow_state = 'approved'
+    end    
+    Document.find(params[:id]).update({'workflow_state'=>@workflow_state})    
+    redirect_to document_path
+  end
+
   # POST /documents or /documents.json
   def create
     parameters = document_params
     cp = Procedure.find(document_params['procedure_id']).code
     cd = DocumentType.find(document_params['document_type_id']).code
     cant = Document.where(document_type_id: parameters['document_type_id'], procedure_id: parameters['procedure_id']).count.to_s
-    parameters[:code] = cp+cd+cant
-    puts "AQUI IMPRIMO LA PRUEBA"
-    puts cant
+    parameters[:code] = cp+cd+cant    
     @document = Document.new(parameters)    
     respond_to do |format|
       if @document.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   resources :document_types
-  resources :documents
+  resources :documents do
+    member do
+      put 'update_workflow_state'
+    end
+  end
   resources :procedures
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  #put 'documents/:id/update_workflow_state', to: 'documents#update_workflow'
 end
